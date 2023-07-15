86a2853fee7cbcd4e9a1357bc69b6b538e49970f
pittu777 <you@example.com>
2023-07-15 15:26:36 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import QRCode from "qrcode.react";
+import "./index.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [qrCodeError, setQrCodeError] = useState(false);
+  const qrCodeRef = useRef();
+
+  const handleInputChange = (event) => {
+    // to handle error
+    const inputValue = event.target.value;
+    if (inputValue.length <= 255) {
+      setInput(inputValue);
+      setQrCodeError(false);
+    } else {
+      setInput("");
+      setQrCodeError(true);
+    }
+  };
+
+  const handleDownloadClick = () => {
+    const canvas = qrCodeRef.current.querySelector("canvas");
+    const dataUrl = canvas.toDataURL("image/png");
+    const downloadLink = document.createElement("a");
+    downloadLink.href = dataUrl;
+    downloadLink.download = "qr-code.png";
+    downloadLink.click();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={handleInputChange}
+        placeholder="Enter text or URL"
+      />
+      {qrCodeError && <p className="error">Data too long....</p>}
+      {!qrCodeError && input && (
+        <div ref={qrCodeRef}>
+          <QRCode value={input} />
+        </div>
+      )}
+      {!qrCodeError && input && (
+        <button onClick={handleDownloadClick}>Download</button>
+      )}
     </div>
   );
 }
